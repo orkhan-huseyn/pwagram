8d3989f5a4f28f0a6954cff1e649dce29ebf2a22
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2019-07-06 15:32:02 +0400

    added app.js to cache for testing

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,18 @@
+var CACHE_VERSION = 1;
+
+var CURRENT_CACHES = {
+  static: 'static-cache-v' + CACHE_VERSION
+};
+
 self.addEventListener('install', function(event) {
   console.log('[Service worker] Installing service worker....', event);
+  event.waitUntil(
+    caches.open(CURRENT_CACHES['static'])
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.add('/src/js/app.js');
+      })
+  );
 });
 
 self.addEventListener('activate', function(event) {
